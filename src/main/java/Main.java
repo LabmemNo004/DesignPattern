cd9203bd29ed7754f31bdef466815293cd612293
pengying <1285524452@qq.com>
2020-11-13 18:20:18 +0800

    Builder+Interpreter+Visitor+Double Check Lock

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import Factory.Factory;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello, world!\n");
+
+        // 初始化农场，根据调用关系先后用到了以下设计模式：
+        // 双重检查锁模式
+        // 建造者模式
+        // 抽象工厂模式
+        // 享元模式
+        // 桥接模式
+        // 多例模式
+        // 私有类数据模式
+        // 组合模式
+        Factory factory = Factory.getInstance();
     }
 }

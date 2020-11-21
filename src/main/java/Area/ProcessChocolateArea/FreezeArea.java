88653601b70cf9c2238a10d9cd469e7b0bb50734
1851788 <1654475161@qq.com>
2020-11-21 20:25:38 +0800

    Update FreezeArea.java

--- src/main/java/Area/ProcessChocolateArea/FreezeArea.java
+++ src/main/java/Area/ProcessChocolateArea/FreezeArea.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 import Color.Color.Colors;
 import Mould.Mould;
 import Factory.Factory;
+import Iterator.ObjectCollection;
 import Chocolate.Chocolate;
 import Area.RawMaterialManagementArea;
 import Worker.LiquidToSolidWorker;
@@ -17,19 +18,27 @@ public class FreezeArea extends WorkerProduceLink{
         List<Chocolate> chocolates = new ArrayList<>();
         for(Worker w:getWorkers()){
             LiquidToSolidWorker worker = (LiquidToSolidWorker)w;
-            // if(moulds.size()==0)break;//没有模具
             Chocolate chocolate = liquid.poll();//获取液体巧克力
             if(chocolate==null)break;
-            int mouldIdx = (int)Math.random()*100%moulds.size();//随机获取模具列表的索引
+            freezeChocolate(worker, chocolates, liquid, chocolate);
+            if(worker.GetExtensionWorker("LiquidToSolid")!=null){
+                Chocolate _chocolate = liquid.poll();//获取液体巧克力
+                if(_chocolate==null)break;
+                freezeChocolate(worker, chocolates, liquid, _chocolate);
+            }
+        }
+        return chocolates;
+    }
+
+    private boolean freezeChocolate(LiquidToSolidWorker worker,List<Chocolate> chocolates,Queue<Chocolate> liquid,Chocolate chocolate){
+        int mouldIdx = (int)Math.random()*100%moulds.size();//随机获取模具列表的索引
             Mould mould = moulds.get(mouldIdx);//获取对应模具
             // chocolate.produce(mould);//赋值
             worker.work(chocolate,mould);//生产工人将巧克力从液体变为固体
             //减少原料
             if(mould.getColor()==Colors.black)area.blackProduce();
             else area.whiteProduce();
-            System.out.println(worker.getName()+"生产了一份巧克力");
+            System.out.println("生产了一份巧克力");
             chocolates.add(chocolate);
-        }
-        return chocolates;
     }
 }

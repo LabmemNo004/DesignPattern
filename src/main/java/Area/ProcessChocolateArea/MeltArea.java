7ddb0e779b0d067779eb8c6109a19c198bdb44da
only976 <42378887+only976@users.noreply.github.com>
2020-11-22 19:20:45 +0800

    Add files via upload

--- src/main/java/Area/ProcessChocolateArea/MeltArea.java
+++ src/main/java/Area/ProcessChocolateArea/MeltArea.java
@@ -12,6 +12,10 @@ public class MeltArea extends WorkerProduceLink{
     public List<Chocolate> melt(Queue<Chocolate> powder){
         List<Chocolate> chocolates = new ArrayList<>();
         for(Worker w:getWorkers()){
+            if(w.getClass()!=PowderToLiquidWorker.class)
+            {
+                w= new Adapter(w);
+            }
             PowderToLiquidWorker worker = (PowderToLiquidWorker)w;
             Chocolate chocolate = powder.poll();//获取下一个巧克力
             if(chocolate==null)break;//可能出现人多巧克力少的情况

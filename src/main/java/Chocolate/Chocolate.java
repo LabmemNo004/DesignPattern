c387b3312edab7fdaa884f0cea531901729d0135
NeKoSaNnn <1427466423@qq.com>
2020-11-22 11:26:27 +0800

    update

--- src/main/java/Chocolate/Chocolate.java
+++ src/main/java/Chocolate/Chocolate.java
@@ -139,15 +139,6 @@ public class Chocolate extends Items implements IChocolate,Colleague{
         this.color=color;
     }
 
-
-    @Override
-    public void setPrice() {//巧克力初始价格
-        double sizePrice= Parameter.chocolatePrice.get(size.toString());
-        double shapePrice= Parameter.chocolatePrice.get(shape.toString());
-        double colorPrice= Parameter.chocolatePrice.get(color.toString());
-        price=sizePrice+shapePrice+colorPrice;
-    }
-
     public void setPrice(double p){//Charlie作为工厂的管理者，有权修改某款巧克力的价格，通过ChococlateController
         price=p;
     }

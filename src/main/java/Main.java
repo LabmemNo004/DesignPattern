8f6380c4c235d72f795e0b3405d98c259aaf833c
Firstname Lastname <43996310+DeBroglie99@users.noreply.github.com>
2020-11-26 00:22:42 +0800

    add main,modify some classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,6 +39,8 @@ import Proxy.*;
 import Shaped.*;
 import SolveStarvation.*;
 import Specification.*;
+import Specification.DetailedSpecification.BigSizeSpecification;
+import Specification.DetailedSpecification.HighQualitySpecification;
 import State.*;
 import Strategy.*;
 import Visitor.*;
@@ -160,6 +162,100 @@ public class Main {
         System.out.println(factory.getChocolates().size());
 
         //责任链模式
+        System.out.println("======== 使用 责任链Responsibility 模式 ========");
+        System.out.println("");
+
+
+        //规约模式
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+
+        String[] chocolateTag ={
+                "质量为低的大巧克力",
+                "质量为低的中巧克力",
+                "质量为低的小巧克力",
+                "质量为中的大巧克力",
+                "质量为中的中巧克力",
+                "质量为中的小巧克力",
+                "质量为高的大巧克力",
+                "质量为高的中巧克力",
+                "质量为高的小巧克力"
+        };
+        Chocolate[] chocolates={new Chocolate(),new Chocolate(),new Chocolate(),new Chocolate(),new Chocolate()
+                ,new Chocolate(),new Chocolate(),new Chocolate(),new Chocolate()};
+        chocolates[0].setQuality(1);chocolates[0].setSize(Mould.Size.big);
+        chocolates[1].setQuality(1);chocolates[1].setSize(Mould.Size.middle);
+        chocolates[2].setQuality(1);chocolates[2].setSize(Mould.Size.small);
+        chocolates[3].setQuality(2);chocolates[3].setSize(Mould.Size.big);
+        chocolates[4].setQuality(2);chocolates[4].setSize(Mould.Size.middle);
+        chocolates[5].setQuality(2);chocolates[5].setSize(Mould.Size.small);
+        chocolates[6].setQuality(3);chocolates[6].setSize(Mould.Size.big);
+        chocolates[7].setQuality(3);chocolates[7].setSize(Mould.Size.middle);
+        chocolates[8].setQuality(3);chocolates[8].setSize(Mould.Size.small);
+
+        for(int i=0;i<chocolates.length;i++)
+        {
+            chocolates[i].getSize();
+            System.out.println("生成巧克力"+chocolateTag[i]+"@"+chocolates[i].hashCode());
+
+        }
+
+        BigSizeSpecification isBig= new BigSizeSpecification();
+        HighQualitySpecification isHighQua=new HighQualitySpecification();
+        System.out.println("新建规约类 BigSizeSpecification@"+isBig.hashCode()+" 和HighQualitySpecification@"+isHighQua.hashCode());
+
+        System.out.println("组合得到四个具体规约法则。");
+        Specification big_high = isBig.and(isHighQua);
+        System.out.println("big_high@"+big_high.hashCode());
+        Specification big_not_high = isBig.and(isHighQua.not());
+        System.out.println("big_not_high@"+big_not_high.hashCode());
+        Specification not_big_high = isBig.not().and(isHighQua);
+        System.out.println("not_big_high@"+not_big_high.hashCode());
+        Specification not_big_not_high = isBig.not().and(isHighQua.not());
+        System.out.println("not_big_not_high@"+not_big_not_high.hashCode());
+
+        System.out.println("开始筛选：");
+        System.out.println("1. 使用规约模式big_high筛选质量高的大巧克力。");
+        for(int i=0;i<chocolates.length;i++){
+            if(big_high.isSatisfiedBy(chocolates[i])){
+                System.out.println("筛选出:"+chocolateTag[i]+" @"+chocolates[i].hashCode());
+            }
+        }
+        System.out.println("2. 使用规约模式big_not_high筛选质量高的中小巧克力。");
+        for(int i=0;i<chocolates.length;i++){
+            if(big_not_high.isSatisfiedBy(chocolates[i])){
+                System.out.println("筛选出:"+chocolateTag[i]+" @"+chocolates[i].hashCode());
+            }
+        }
+        System.out.println("3. 使用规约模式not_big_high筛选质量高的中小巧克力。");
+        for(int i=0;i<chocolates.length;i++){
+            if(not_big_high.isSatisfiedBy(chocolates[i])){
+                System.out.println("筛选出:"+chocolateTag[i]+" @"+chocolates[i].hashCode());
+            }
+        }
+        System.out.println("4. 使用规约模式not_big_not_high筛选质量为中低的中小巧克力。");
+        for(int i=0;i<chocolates.length;i++){
+            if(not_big_not_high.isSatisfiedBy(chocolates[i])){
+                System.out.println("筛选出:"+chocolateTag[i]+" @"+chocolates[i].hashCode());
+            }
+        }
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+        System.out.println("==================================");
+
+
+
 
 
         //原型模式
@@ -188,7 +284,7 @@ public class Main {
         director.setBuilder( liquidToSolidWorkerBuilder );
         director.constructWorker();
         Worker worker2 = director.getWorker();
-        System.out.println( director.toStringZh() );
+        System.out.println( director.toStringZh());
         System.out.println("");
 
         //转换器模式

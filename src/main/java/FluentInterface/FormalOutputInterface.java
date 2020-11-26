59f1684c67db01f88556817de38dec2bb36d915c
Firstname Lastname <43996310+DeBroglie99@users.noreply.github.com>
2020-11-26 23:45:58 +0800

    FluentInterface

---
+++ src/main/java/FluentInterface/FormalOutputInterface.java
@@ -0,0 +1,33 @@
+package FluentInterface;
+
+public class FormalOutputInterface extends FluentInterface{
+
+    private String[] from;
+
+    private String[] where;
+
+    public FormalOutputInterface from(String... from) {
+        this.from = from;
+
+        return this;
+    }
+
+    public FormalOutputInterface where(String... where) {
+        this.where = where;
+
+        return this;
+    }
+
+    @Override
+    public String getOutput() {
+        StringBuilder sqlBuilder = new StringBuilder();
+        sqlBuilder.append("SELECT ")
+                .append("chocolate")
+                .append(" FROM ")
+                .append(String.join("", from))
+                .append(". Attribute: ")
+                .append(String.join(" AND ", where));
+        return sqlBuilder.toString();
+    }
+
+}
---
+++ src/test/java/FluentInterfaceTest.java
@@ -0,0 +1,14 @@
+import FluentInterface.FormalOutputInterface;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+public class FluentInterfaceTest {
+    @Test
+    public void main()
+    {
+        FormalOutputInterface sql = new FormalOutputInterface();
+        sql.from("ChocolateSellArea").where("quality=2", "size=big","color=white");
+        String query = sql.getOutput();
+        System.out.println(query);
+    }
+}

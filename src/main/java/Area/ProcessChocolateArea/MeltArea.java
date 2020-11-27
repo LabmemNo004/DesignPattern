2835981bd9590eabe84a96778c8cb01a27cdcd05
only976 <42378887+only976@users.noreply.github.com>
2020-11-27 15:59:57 +0800

    Add files via upload

--- src/main/java/Area/ProcessChocolateArea/MeltArea.java
+++ src/main/java/Area/ProcessChocolateArea/MeltArea.java
@@ -6,33 +6,46 @@ import java.util.Queue;
 
 import Chocolate.Chocolate;
 import Worker.PowderToLiquidWorker;
-import Worker.*;
+import Worker.Worker;
+import Chocolate.IChocolate;
+import Worker.LiquidToSolidWorker;
+import Worker.Adapter;
+import Worker.Extension;
+import Worker.ConcreteExtension;
 
 public class MeltArea extends WorkerProduceLink{
-    public List<Chocolate> melt(Queue<Chocolate> powder){
-        List<Chocolate> chocolates = new ArrayList<>();
-        for(Worker w:getWorkers()){
+    public List<IChocolate> melt(Queue<IChocolate> powder){
+        List<IChocolate> chocolates = new ArrayList<>();
+        OUTER:
+        for(Object w:getWorkers()){
             if(w.getClass()==LiquidToSolidWorker.class)
             {
-                w= new Adapter(w);
+                w= new Adapter((LiquidToSolidWorker)w);
+                System.out.println("======== 使用适配器模式 ========");
+                System.out.println("======== 适配了一个工人 ========");
+
             }
-            PowderToLiquidWorker worker = (PowderToLiquidWorker)w;
-            Chocolate chocolate = powder.poll();//获取下一个巧克力
-            if(chocolate==null)break;//可能出现人多巧克力少的情况
-            meltChocolate(worker, chocolates, chocolate);
-            if(worker.getWorkTypeString().equals("SuperPowderToLiquid")){//出现强化工人
-                Chocolate _chocolate = powder.poll();//获取下一个巧克力
-                if(_chocolate==null)break;//可能出现人多巧克力少的情况
-                meltChocolate(worker, chocolates, _chocolate);
+            if(w instanceof Worker){//普通工人
+                PowderToLiquidWorker worker = (PowderToLiquidWorker)w;
+                IChocolate chocolate = powder.poll();//获取下一个巧克力
+                if(chocolate==null)break;//可能出现人多巧克力少的情况
+                meltChocolate(worker, chocolates, chocolate);
             }
-            
+            else{//强化工人
+                Extension extension = (ConcreteExtension)w;
+                for (int i = 0; i < 2; i++) {
+                    IChocolate chocolate = powder.poll();//获取下一个巧克力
+                    if(chocolate==null)break OUTER;//可能出现人多巧克力少的情况
+                    meltChocolate((PowderToLiquidWorker)extension.getOwner(), chocolates, chocolate);
+                }
+            }
         }
         return chocolates;
     }
 
-    private void meltChocolate(PowderToLiquidWorker worker,List<Chocolate> chocolates,Chocolate chocolate) {
-        worker.work(chocolate);//生产工人将巧克力从粉末变成液体
+    private void meltChocolate(PowderToLiquidWorker worker,List<IChocolate> chocolates,IChocolate chocolate) {
+        worker.work((Chocolate)chocolate);//生产工人将巧克力从粉末变成液体
         System.out.println("融化了一份巧克力");
-        chocolates.add(chocolate);
+        chocolates.add((IChocolate)chocolate);
     }
 }

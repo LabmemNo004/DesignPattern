32b58b059bc6003bdedc511931c7b74e0ae26333
pengying <1285524452@qq.com>
2020-11-27 22:12:04 +0800

    流接口test输出

--- src/test/java/FluentInterfaceTest.java
+++ src/test/java/FluentInterfaceTest.java
@@ -7,9 +7,10 @@ public class FluentInterfaceTest {
     @Test
     public void main()
     {
+        System.out.println("=====使用流接口模式======");
         FormalOutputInterface sql = new FormalOutputInterface();
         sql.from("ChocolateSellArea").where("quality=2", "size=big","color=white");
-        //String query = sql.getOutput();
-        //System.out.println(query);
+        String query = sql.getQuery();
+        System.out.println(query);
     }
 }

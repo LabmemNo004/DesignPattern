8c66a982abaadb443726c3eb95b81d80853b22bc
NeKoSaNnn <1427466423@qq.com>
2020-11-27 12:34:13 +0800

    update

--- src/main/java/BusinessDelegate/Business/DisplayWorkerState.java
+++ src/main/java/BusinessDelegate/Business/DisplayWorkerState.java
@@ -19,8 +19,8 @@ public class DisplayWorkerState implements BusinessService{
         System.out.println("=======通过终端执行\"查看当前工人工作状态=======");
 
 
-        System.out.println("当前忙碌工人数量为:"+freeList.size());
-        System.out.println("当前空闲工人个数为:"+busyList.size());
+        System.out.println("当前忙碌工人数量为:"+busyList.size());
+        System.out.println("当前空闲工人个数为:"+freeList.size());
 
         try {
             TimeUnit.SECONDS.sleep(1);

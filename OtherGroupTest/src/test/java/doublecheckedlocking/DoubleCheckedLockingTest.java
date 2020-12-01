0038650b15adf105217e73d2e22d64d731932b30
Firstname Lastname <43996310+DeBroglie99@users.noreply.github.com>
2020-12-01 19:29:17 +0800

    update test by hzj

--- OtherGroupTest/src/test/java/doublecheckedlocking/DoubleCheckedLockingTest.java
+++ OtherGroupTest/src/test/java/doublecheckedlocking/DoubleCheckedLockingTest.java
@@ -4,25 +4,23 @@ import org.junit.Test;
 
 
 /**
- * Author: CINDY DENG
- * Date: 2020/11/12,22:57
- * Version: 1.0
+ * Author: hzj
+ * Time: 2020-12-01
  */
 public class DoubleCheckedLockingTest {
 
 
     @Test
     public void test(){
-        System.out.println("---------------- [Pattern] DoubleCheckedLocking ----------------");
+        System.out.println("----------------Test [Pattern] DoubleCheckedLocking ----------------");
         MyThread[] threads = new MyThread[10];
         for(int i = 0;i<10;i++){
             threads[i] = new MyThread(i);
         }
 
         for(MyThread thread : threads){
-
             thread.run();
         }
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("------------------------ Over ------------------------");
     }
 }

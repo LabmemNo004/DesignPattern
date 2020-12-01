edb8424fb7f16b268ff6436e5c8377694de22ea9
LabmemNo004 <1085201315@qq.com>
2020-12-01 18:49:20 +0800

    test

--- OtherGroupTest/src/test/java/chainofresponsibility/ChainOfResponsibilityTest.java
+++ OtherGroupTest/src/test/java/chainofresponsibility/ChainOfResponsibilityTest.java
@@ -3,17 +3,29 @@ package chainofresponsibility;
 import entity.AbstractReferees;
 import org.junit.Test;
 
+/**
+ * @Tester LabmemNo004
+ * @Date: 17:56 2020/12/1
+ **/
+
 public class ChainOfResponsibilityTest {
     /**
      * 构造测试用例
      */
     private static final AbstractReferees referees = SetReferees.SetReferees("Unknown","Unknown",true);
+    private static final AbstractReferees referees1 = SetReferees.SetReferees("Unknown","Goal",true);
+    private static final AbstractReferees referees2 = SetReferees.SetReferees("Goal","Unknown",true);
 
     @Test
     public void ChainOfResponsibilityTest() {
-        System.out.println("---------------- [Pattern] ChainOfResponsibility ----------------");
+        System.out.println("=============== TEST [ChainOfResponsibility] ===============");
+        System.out.println("=============== TEST 1 ===============");
         referees.judgement();
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("=============== TEST 2 ===============");
+        referees1.judgement();
+        System.out.println("=============== TEST 3 ===============");
+        referees2.judgement();
+        System.out.println("=============== TEST END ===============");
     }
 
 

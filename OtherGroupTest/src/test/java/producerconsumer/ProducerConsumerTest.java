56fc958abb6f9bc6b5089308c2f772c877c673a7
WEkai <49737783+WEkai@users.noreply.github.com>
2020-12-01 15:56:23 +0800

    测试

--- OtherGroupTest/src/test/java/producerconsumer/ProducerConsumerTest.java
+++ OtherGroupTest/src/test/java/producerconsumer/ProducerConsumerTest.java
@@ -4,37 +4,37 @@ import entity.Table;
 import org.junit.Test;
 
 /**
- * Author: CINDY DENG 1850477
- * Date: 2020/11/23,19:28
+ * Author: Wenkai Tian
+ * Date: 2020/12/1,15:45
  * Version: 1.0
  */
 public class ProducerConsumerTest {
 
     @Test
     public void producerConsumerTest() {
-        System.out.println("---------------- [Pattern] Producer-Customer ----------------");
+        System.out.println("====== 测试 Producer-Customer设计模式 ======");
 
         Table table = new Table(2);
-        MakerThread m1 = new MakerThread("MakerThread-1",table,31415);
-        MakerThread m2 = new MakerThread("MakerThread-2",table,12345);
-        DrinkerThread d1 = new DrinkerThread("DrinkerThread-2",table,33333);
-        DrinkerThread d2 = new DrinkerThread("DrinkerThread-1",table,76536);
+        MakerThread makerThreadTest1 = new MakerThread("makerThreadTest1",table,126);
+        MakerThread makerThreadTest2 = new MakerThread("makerThreadTest2",table,3579);
+        DrinkerThread drinkerThreadTest1 = new DrinkerThread("drinkerThreadTest1",table,111);
+        DrinkerThread drinkerThreadTest2 = new DrinkerThread("drinkerThreadTest2",table,678);
 
         try{
-            m1.start();
-            m2.start();
-            d1.start();
-            d2.start();
-            m1.join();
-            m2.join();
-            d1.join();
-            d2.join();
+            makerThreadTest1.start();
+            makerThreadTest2.start();
+            drinkerThreadTest1.start();
+            drinkerThreadTest2.start();
+            makerThreadTest1.join();
+            makerThreadTest2.join();
+            drinkerThreadTest1.join();
+            drinkerThreadTest2.join();
         }catch (Exception e){
             e.printStackTrace();
         }
 
 
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("====== 测试完毕 ======");
     }
 
 

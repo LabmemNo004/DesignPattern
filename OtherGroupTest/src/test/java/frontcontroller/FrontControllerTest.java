71df63d293359ed167a2d78823ad3cefd4e1ea49
=Jasmine_D <=806193587@qq.com>
2020-12-01 21:28:56 +0800

    1,11,21,31,41修改test

--- OtherGroupTest/src/test/java/frontcontroller/FrontControllerTest.java
+++ OtherGroupTest/src/test/java/frontcontroller/FrontControllerTest.java
@@ -2,26 +2,38 @@ package frontcontroller;
 
 import org.junit.Test;
 
+/**
+ * @author Jasmine
+ * @Date Created in 20:50 2020/12/1
+ **/
+
 public class FrontControllerTest {
 
     @Test
     public void frontControllerTest(){
 
-        System.out.println("---------------- [Pattern] FrontController ----------------");
+        System.out.println("----------------测试设计模式： FrontController 前端控制器模式----------------");
 
         FrontController start=new FrontController();
-        //代表验证管理员身份
+        
+        /**
+         *代表验证其他人身份，名字为Jasmine
+         */
+        start.identityVerification("Jasmine");
+        System.out.println("");
+        
+        /**
+         *代表验证运动员身份，名字为Kant
+         */
+        start.identityVerification("Kant");
+        System.out.println("");
+        
+        /**
+         *代表验证管理员身份，名字为root
+         */
         start.identityVerification("root");
         System.out.println("");
-        //代表验证运动员身份
-        start.identityVerification("Healy");
-        System.out.println("");
-
-        //代表验证其他人身份
-        start.identityVerification("people");
-        System.out.println("");
-        System.out.println("------------------------ END ------------------------");
-
+        System.out.println("------------------------------- END ----------------------------------");
     }
 
 }
--- OtherGroupTest/src/test/java/observer/ObserverTest.java
+++ OtherGroupTest/src/test/java/observer/ObserverTest.java
@@ -3,17 +3,20 @@ package observer;
 import org.junit.Test;
 
 /**
- * description: 观察者模式测试类
- * date: 11/11/20 2:58 AM
- * author: fourwood
- */
+ * @author Jasmine
+ * @Date Created in 21:10 2020/12/1
+ **/
 public class ObserverTest {
 
     @Test
     public void observerTest(){
 
-        System.out.println("---------------- [Pattern] Observer ----------------");
+        System.out.println("----------------测试设计模式：Observer 观察者模式----------------");
+        
+        /**
+         *创建两个比赛:游泳和跑步，每个比赛各创建裁判1和运动员2，随后裁判宣布比赛开始，通知到运动员
+         */
         ObserverDemo.observerTest();
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("----------------------------- END -----------------------------");
     }
 }

82df011b83e3aeef0210e75eaaa91ab8d4b82d0c
NeKoSaNnn <1427466423@qq.com>
2020-12-01 16:20:25 +0800

    updateTest

--- OtherGroupTest/src/test/java/singleton/SingletonTest.java
+++ OtherGroupTest/src/test/java/singleton/SingletonTest.java
@@ -2,19 +2,24 @@ package singleton;
 
 import org.junit.Test;
 
+/**
+ * @Tester Neko
+ * @Date: 16:08 2020/12/1
+ **/
+
 public class SingletonTest {
     @Test
     public void SingletonTest(){
-        System.out.println("---------------- [Pattern] Singleton ----------------");
-        Sponsor thisSponsor = Sponsor.getInstance();
-        Sponsor thatSponsor = Sponsor.getInstance();
-        if(thisSponsor == thatSponsor) {
-            System.out.println("This Sponsor("+thisSponsor.toString()+") and that Sponsor("+thatSponsor.toString()+") are the same one.");
-            System.out.println("The Sponsor's name is: "+thisSponsor.getName()+"");
+        System.out.println("=============== TEST [Singleton] ===============");
+        Sponsor Sponsor1 = Sponsor.getInstance();
+        Sponsor Sponsor2 = Sponsor.getInstance();
+        if(Sponsor1 == Sponsor2) {
+            System.out.println("=====Create Sponsor1("+Sponsor1.toString()+") and Sponsor2("+Sponsor2.toString()+") are the same one.=====");
+            System.out.println("=====The Sponsor's name is: "+Sponsor1.getName()+"=====");
         }
         else {
-            System.out.println("This Sponsor("+thisSponsor.toString()+") and that Sponsor("+thatSponsor.toString()+") are not the same one.");
+            System.out.println("=====Sponsor1("+Sponsor1.toString()+") and Sponsor2("+Sponsor2.toString()+") are not the same one.=====");
         }
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("=============== TEST END ===============");
     }
 }

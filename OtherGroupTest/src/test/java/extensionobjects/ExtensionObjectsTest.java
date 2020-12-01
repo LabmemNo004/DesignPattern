3499d511264da90e1b865163a692132d1107f7bc
only976 <42378887+only976@users.noreply.github.com>
2020-12-01 22:21:42 +0800

    Add files via upload

--- OtherGroupTest/src/test/java/extensionobjects/ExtensionObjectsTest.java
+++ OtherGroupTest/src/test/java/extensionobjects/ExtensionObjectsTest.java
@@ -2,16 +2,21 @@ package extensionobjects;
 
 import org.junit.Test;
 
+/**
+ * @TestBy: 1852346
+ * @TestTime: 2020/12/01 21:00
+ */
+
 public class ExtensionObjectsTest {
     @Test
 
     public void ExtensionObjectsTest (){
-        System.out.println("---------------- [Pattern] ExtensionObjects ----------------");
+        System.out.println("----------------[test] [Pattern] ExtensionObjects ----------------");
         Race race = new ConcreteRace();
         Overtime overtimeInjury = race.GetExtension("Injury");
         Overtime overtimeDraw = race.GetExtension("Draw");
         overtimeInjury.SetOvertime(5);
         overtimeDraw.SetOvertime(15);
-        System.out.println("------------------------ END ------------------------");
+        System.out.println("------------------------ [test]END ------------------------");
     }
 }

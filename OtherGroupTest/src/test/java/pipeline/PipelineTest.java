6dcb84ad84cc361caf41cc8dad2c5a07c8ec7074
kuronekowww <49890629+kuronekowww@users.noreply.github.com>
2020-12-02 13:16:20 +0800

    Update PipelineTest.java

--- OtherGroupTest/src/test/java/pipeline/PipelineTest.java
+++ OtherGroupTest/src/test/java/pipeline/PipelineTest.java
@@ -1,10 +1,13 @@
 package pipeline;
 
+import org.junit.Test;
+
 /**
  * @Author: wjm
  * @Date: Created in 2020/12/01
  */
 public class PipelineTest {
+    @Test
     public void pipelineTest(){
         System.out.println("===========Pipeline Test===========");
         PipelineDemo.pipelineTest();
